4103c45a54297be186dd8582830cd52454aaab3f
taaara <erjyu20@bergen.org>
2020-01-27 13:25:39 -0500

    started students.models

--- app/bakesale/views.py
+++ app/bakesale/views.py
@@ -1,16 +1,15 @@
-from app.elective import elective_mod
+from app.bakesale import bakesale_mod
 
 from app.shared.controllers import requires_token
 
 from flask import g, redirect, url_for
 
-
-@elective_mod.before_request
+@bakesale_mod.before_request
 @requires_token
 def check_auth():
     print("checking token....")
 
-@elective_mod.route('/')
+@bakesale_mod.route('/')
 def index():
     type_code = g.user.get_role('ELEN')
 
