7b254f42f95842378f5444aaa8f6818745af248b
taaara <erjyu20@bergen.org>
2020-04-13 08:54:08 -0400

    worked on teacher's "Create Request" page

--- app/bakesale/teacher/controllers.py
+++ app/bakesale/teacher/controllers.py
@@ -45,7 +45,6 @@ def get_teacher_bakesales(teacher_id):
                                         " where bakesale.status_code = status.status_code "
                                         " and teacher_id = bakesale.teacher_id "
                                         " and bakesale.teacher_id = t.usr_id "
-
                                         " order by bakesale_id")
 
     e_bakesales = []
@@ -73,4 +72,5 @@ def get_teacher_bakesales(teacher_id):
 def create_bakesale(group_name, group_size, items_desc, requested_day, teacher_id):
     insert(DB.BAKESALE, "INSERT INTO bakesale (group_name, group_size, items_desc, requested_day, teacher_id) VALUES (%s, %s, %s, %s, %s)", (group_name, group_size, items_desc, requested_day, teacher_id))
 
-    return query_one(DB.BAKESALE, "SELECT bakesale_id FROM bakesale WHERE group_name=%s AND `items_desc`=%s", [group_name, items_desc])[0]
+    return query_one(DB.BAKESALE, "SELECT bakesale_id FROM bakesale")[0]
+
