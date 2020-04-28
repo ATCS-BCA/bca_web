62fa02e17ee2ee440251532595cb8c5a215bcee4
Eleana Pardo <eleanajpardo@gmail.com>
2020-04-27 20:09:41 -0400

    next 5 bakesales

--- app/bakesale/student/controllers.py
+++ app/bakesale/student/controllers.py
@@ -1,10 +1,11 @@
 from app.db import DB, insert, insertmany, query_one, query, delete, update
 
+
 from app.bakesale.student.models import Bakesale
 
 from .util import datetime_from_string, us_format
 
-from datetime import datetime
+import datetime
 
 # Get all current elective sections for a tri/year
 def get_all_bakesales():
@@ -37,12 +38,12 @@ def get_all_bakesales():
 
 # Temporary solution for getting the corresponding times for a section
 def get_current_date():
-    d = datetime.datetime.today()
+    d = str(datetime.date.today())
     return d
 
 
 # Temporary solution for getting the corresponding times for a section
 def get_next_bakesales():
-    result = query(DB.BAKESALE, "SELECT * FROM bakesale WHERE date >= " + get_current_date() + " LIMIT 5")
+    result = query(DB.BAKESALE, "SELECT bakesale_id, group_name, group_size, teacher_id, requested_day, items_desc, date, status_code, DATE_FORMAT(date, '%M %D') as formatted_date FROM bakesale WHERE date >= " + get_current_date() + " ORDER BY date LIMIT 5")
     return result
 
