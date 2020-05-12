cff36bfa022e34184113f89e255168509b53313a
nahkim20 <nahkim20@bergen.org>
2020-05-12 14:24:31 -0400

    clubs student page error

--- app/mclub/__init__.py
+++ app/mclub/__init__.py
@@ -6,8 +6,8 @@ import app.mclub.views
 
 from .admin import admin_mod
 from .teacher import teacher_mod
-# from .student import student_mod
+from .student import student_mod
 
 mclub_mod.register_blueprint(admin_mod)
 mclub_mod.register_blueprint(teacher_mod)
-# mclub_mod.register_blueprint(student_mod)
+mclub_mod.register_blueprint(student_mod)
--- app/mclub/student/views.py
+++ app/mclub/student/views.py
@@ -3,16 +3,71 @@ from app.mclub.student import student_mod
 from app.shared.controllers import requires_token
 from app.mclub.student.controllers import *
 
-from flask import g, redirect, url_for, render_template, request, jsonify, make_response
+from flask import g, redirect, url_for, render_template, request, jsonify
 
+# Checks whether the requester
 @student_mod.before_request
 @requires_token
 def check_teacher():
-    pass
-    #if g.user.get_role('CLUB') != 'ADM':
-    #  return redirect(url_for('mclub.index'))
+    if g.user.get_role('CLUB') != 'STD':
+        return redirect(url_for('mclub'))
 
+# pages
+
+# A route for the student_mod app
 @student_mod.route('/')
 def index():
-    return render_template("mclub/student/index.html", clubs=get_clubs())
+    enroll_info = get_enrollment_time(g.user.get_grade_level())
 
+    enrolled_clubs = []
+    available_clubs = []
+
+    if not enroll_info.start_time is None:
+        enrolled_clubs = get_enrolled_clubs(g.user.get_id(), enroll_info.course_year, enroll_info.tri_nbr)
+        available_clubs = get_clubs()
+
+    return render_template("mclub/student/index.html", clubs=available_clubs, enroll_info=enroll_info, enrolled_clubs=enrolled_clubs)
+
+
+@student_mod.route('/enroll/<int:id>', methods=['PUT'])
+def enroll(id):
+    data = wrequest.get_json(force=True, silent=True)
+
+    club_id = id
+    usr_id = data['usr_id']
+    will_enroll = data['enroll']
+
+    if enrollment_open(g.user.get_grade_level()):
+        if will_enroll:
+            if not is_club_full(club_id):
+                enroll_user(usr_id, club_id)
+
+                return jsonify({"has_enrolled": True, "Error": None})
+
+            else:
+                return jsonify({"has_enrolled": False, "Error": "Club Full."})
+        else:
+            drop_club(usr_id, club_id)
+            return jsonify({"has_enrolled": False, "Error": None})
+
+    else:
+        return jsonify({"has_enrolled": False, "Error": "Enrollment Not Open"})
+
+
+@student_mod.route('/enroll/update', methods=['PUT'])
+def ping():
+    data = request.get_json(force=True, silent=True)
+
+    club_ids = data['club_ids']
+
+    if(club_ids):
+
+        amount_left = {}
+
+        for i in range(len(club_ids)):
+            amount_left[club_ids[i]] = get_amount_left(club_ids[i])
+
+        return jsonify(amount_left)
+
+    else:
+        return jsonify({"Error": "Invalid parameters"})
