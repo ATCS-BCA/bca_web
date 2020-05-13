a0cf52efa03e5b699d7cca102ae05008cc12ff4c
Eleana Pardo <eleanajpardo@gmail.com>
2020-05-12 23:19:32 -0400

    next 5 bakesales

--- app/bakesale/admin/__init__.py
+++ app/bakesale/admin/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 admin_mod = Blueprint('bakesale_admin', __name__, url_prefix='/admin')
 
-import app.bakesale.student.views
+import app.bakesale.admin.views
---
+++ app/bakesale/admin/models.py
@@ -0,0 +1,9 @@
+class Bakesale(object):
+
+    def __init__(self, id, group_name, group_size, items_desc, date):
+        self.id = id
+        self.group_name = group_name
+        self.group_size = group_size
+        self.items_desc = items_desc
+        self.date = date
+
