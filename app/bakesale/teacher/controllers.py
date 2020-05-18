1fc3d23c438b73b46c33dc65a27d0bde07d9b9ad
taaara <erjyu20@bergen.org>
2020-05-18 11:02:42 -0400

    worked on teacher's "edit bakesale" page

--- app/bakesale/teacher/controllers.py
+++ app/bakesale/teacher/controllers.py
@@ -29,10 +29,10 @@ def get_all_bakesales():
         items_desc = bakesale[5]
         date = bakesale[6]
         status_code = bakesale[7]
-        status_desc = bakesale[8]
+        description = bakesale[8]
         teacher_name = bakesale[9]
 
-        bakesale = Bakesale(bakesale_id, teacher_id, group_name, group_size, items_desc, date, teacher_name)
+        bakesale = Bakesale(bakesale_id, teacher_id, group_name, group_size, items_desc, date, teacher_name, description)
 
         e_bakesales.append(bakesale)
 
@@ -60,10 +60,10 @@ def get_teacher_bakesales(teacher_id):
         items_desc = bakesale[5]
         date = bakesale[6]
         status_code = bakesale[7]
-        status_desc = bakesale[8]
+        description = bakesale[8]
         teacher_name = bakesale[9]
 
-        bakesale = Bakesale(bakesale_id, teacher_id, group_name, group_size, items_desc, date, teacher_name)
+        bakesale = Bakesale(bakesale_id, teacher_id, group_name, group_size, items_desc, date, teacher_name, description)
 
         e_bakesales.append(bakesale)
 
@@ -73,4 +73,24 @@ def create_bakesale(group_name, group_size, items_desc, requested_day, teacher_i
     insert(DB.BAKESALE, "INSERT INTO bakesale (group_name, group_size, `items_desc`, requested_day, teacher_id, status_code) VALUES (%s, %s, %s, %s, %s, %s)", (group_name, group_size, items_desc, requested_day, teacher_id, 'R'))
 
     return query_one(DB.BAKESALE, "SELECT bakesale_id FROM bakesale WHERE group_name=%s AND `items_desc`=%s", [group_name, items_desc])[0]
+
+def get_bakesale(bakesale_id):
+    info = query(DB.BAKESALE, "SELECT bakesale_id, group_name, group_size, teacher_id, requested_day, items_desc, date, bakesale.status_code, description, CONCAT(t.usr_last_name, ', ', t.usr_first_name) as teacher_name "
+                           " FROM bakesale, user t, status" 
+                           " WHERE bakesale.status_code = status.status_code"
+                           " AND bakesale_id = %s ", [bakesale_id])
+
+    if info:
+        b = info[0]
+        bakesale = Bakesale(b[0], b[3], b[1], b[2], b[5], b[6], b[9], b[8])
+        return bakesale
+
+    return None
+
+def edit_bakesale(bakesale_id, group_name, group_size, items_desc, requested_day):
+    query = "UPDATE bakesale SET group_name = %s, group_size = %s, items_desc = %s, requested_day = %s WHERE bakesale_id = %s"
+    params = [group_name, group_size, items_desc, requested_day, bakesale_id]
+    update(DB.BAKESALE, query, params)
+
+    return None
 
