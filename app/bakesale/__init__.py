acc922d21bdc01abcdd4a52bdeebf68f30fac833
daniellacalle <daniella.acalle@gmail.com>
2020-06-05 12:52:32 -0400

    changed the button in rosters

--- app/bakesale/__init__.py
+++ app/bakesale/__init__.py
@@ -5,9 +5,9 @@ bakesale_mod = NestableBlueprint('bakesale', __name__, url_prefix='/bakesale')
 import app.bakesale.views
 
 from .admin import admin_mod
-from .teacher import teacher_mod
+# from .teacher import teacher_mod
 from .student import student_mod
 
 bakesale_mod.register_blueprint(admin_mod)
-bakesale_mod.register_blueprint(teacher_mod)
+#bakesale_mod.register_blueprint(teacher_mod)
 bakesale_mod.register_blueprint(student_mod)
